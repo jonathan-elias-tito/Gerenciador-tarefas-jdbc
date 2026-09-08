b0027b77eeb246e6a30e7c9799c2ddecb88a58da
Jonathan <jonathanelias998@gmail.com>
2026-09-07 23:57:36 -0300

    feat: atualiza interfaces DAO e entidades User/Task

--- src/model/dao/TaskDao.java
+++ src/model/dao/TaskDao.java
@@ -1,5 +1,15 @@
 package model.dao;
 
-public interface TaskDao {
+import java.util.List;
+import model.entities.Task;
+import model.entities.User;
 
+public interface TaskDao {
+	void insert(Task obj);
+	void update (Task obj);
+	void deleteById(Integer Id);
+	List<Task>findAll();
+	List<Task>findById(Task Id);
+	List<Task>findByUser(User user);
+	
 }
